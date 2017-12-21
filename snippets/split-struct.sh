450ba78a838ca2a1e975ac2855ca135fdd3d876f
Domenico Di Iorio <ddiiorio@jmawireless.com>
2017-12-21 06:19:52 -0500

    improved split-struct for other syntax

--- snippets/split-struct.sh
+++ snippets/split-struct.sh
@@ -1,6 +1,14 @@
 START=$(cat $1 | grep -nE "(struct +$2 +{|})" | grep -nEA1 "struct +$2 +{" | tr ":" "\n" | tr "-" "\n" | sed -n '2,2p')
 END=$(cat $1 | grep -nE "(struct +$2 +{|})" | grep -nEA1 "struct +$2 +{" | tr ":" "\n" | tr "-" "\n" | sed -n '5,5p')
 
+if [ -z "$START" -a "${START+xxx}" = "xxx" ];
+then
+
+START=$(cat $1 | grep -nE "(struct +{|} +$2)" | grep -nEB1 "$2" | tr ":" "\n" | tr "-" "\n" | sed -n '2,2p')
+END=$(cat $1 | grep -nE "(struct +{|} +$2)" | grep -nEB1 "$2" | tr ":" "\n" | tr "-" "\n" | sed -n '5,5p')
+
+fi
+
 echo "/*"
 echo "start:$START"
 echo "end:$END"
