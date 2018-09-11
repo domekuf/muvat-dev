405f865432a6b5a99edddd121ebca8c9fa1ae75b
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-09-11 09:58:44 +0000

    MUVAT install first draft

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ mkdir $HOME/.muvat/git
 cp -r config.d $HOME/.muvat/git/
 git config --global include.path $HOME/.muvat/git/config.d/alias
 
+## .bashrc START ##
+echo "## MUVAT START ##" >> $HOME/.bashrc
+echo "#### Don't delete this section. Use \`muvat uninstall\` instead" >> $HOME/.bashrc
 ## JMA START ##
 mkdir $HOME/.muvat/jma
 ln -f jma/_bashrc $HOME/.muvat/jma/bashrc
+echo "source .muvat/jma/bashrc" >> $HOME/.bashrc
 ## JMA STOP ##
+echo "## MUVAT STOP ##" >> $HOME/.bashrc
+## .bashrc STOP ##
