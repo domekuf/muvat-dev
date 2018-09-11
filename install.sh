2e75b303156f215e4cb9c836858ee64ea85daea9
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-09-11 09:47:43 +0000

    jma install

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ mkdir $HOME/.muvat/
 mkdir $HOME/.muvat/git
 cp -r config.d $HOME/.muvat/git/
 git config --global include.path $HOME/.muvat/git/config.d/alias
+
+## JMA START ##
+mkdir $HOME/.muvat/jma
+ln -f jma/_bashrc $HOME/.muvat/jma/bashrc
+## JMA STOP ##
