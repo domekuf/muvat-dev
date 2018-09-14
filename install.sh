ad8906712074deb120dcfb99f2a09caf9ca7f1cb
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-09-14 09:37:31 +0000

    Git completion included

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 git submodule init
 git submodule update
+
 mkdir $HOME/.muvat/
 mkdir $HOME/.muvat/git
+
 cp -r config.d $HOME/.muvat/git/
 git config --global include.path $HOME/.muvat/git/config.d/alias
+
+cp git-completion.bash $HOME/.muvat/git/
+echo "source $HOME/.muvat/git/git-completion.bash" >> $HOME/.bashrc
+
 ln -f uninstall.sh $HOME/.muvat/uninstall.sh
 ln -f _bashrc $HOME/.muvat/bashrc
 
@@ -11,14 +17,14 @@ ln -f _bashrc $HOME/.muvat/bashrc
 echo "## MUVAT START ##" >> $HOME/.bashrc
 echo "#### Don't delete this section. Use \`muvat-uninstall\` instead" >> $HOME/.bashrc
 echo "source $HOME/.muvat/bashrc" >> $HOME/.bashrc
-source $HOME/.muvat/bashrc
 
 ## JMA START ##
 mkdir $HOME/.muvat/jma
 ln -f jma/_bashrc $HOME/.muvat/jma/bashrc
 echo "source $HOME/.muvat/jma/bashrc" >> $HOME/.bashrc
-source $HOME/.muvat/jma/bashrc
 ## JMA STOP ##
 
 echo "## MUVAT STOP ##" >> $HOME/.bashrc
 ## .bashrc STOP ##
+
+source $HOME/.bashrc
