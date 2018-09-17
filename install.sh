a264a72454ebcbefb6c3ff56bfff82326d9203d8
Domenico Di Iorio <dome.diiorio@gmail.com>
2018-09-17 14:34:17 +0000

    Fixed git completion installation

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ mkdir $HOME/.muvat/git
 
 cp -r config.d $HOME/.muvat/git/
 git config --global include.path $HOME/.muvat/git/config.d/alias
+cp git-completion.bash $HOME/.muvat/git/
+# source is done in _allrc/_bashrc
 
 ln -f uninstall.sh $HOME/.muvat/uninstall.sh
-ln -f _bashrc $HOME/.muvat/bashrc
+ln -f _allrc/_bashrc $HOME/.muvat/bashrc
 
 ## .bashrc START ##
 echo "## MUVAT START ##" >> $HOME/.bashrc
@@ -21,8 +23,6 @@ ln -f jma/_bashrc $HOME/.muvat/jma/bashrc
 echo "source $HOME/.muvat/jma/bashrc" >> $HOME/.bashrc
 ## JMA STOP ##
 
-cp git-completion.bash $HOME/.muvat/git/
-echo "source $HOME/.muvat/git/git-completion.bash" >> $HOME/.bashrc
 echo "## MUVAT STOP ##" >> $HOME/.bashrc
 ## .bashrc STOP ##
 
